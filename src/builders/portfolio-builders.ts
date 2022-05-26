483f064412e5257469d9481dd8067181027ce0fc
Tayden Flitcroft <taydengoodeill@gmail.com>
2022-05-26 01:38:28 -0500

    adding resume and pdf worker endpoint

--- src/builders/portfolio-builders.ts
+++ src/builders/portfolio-builders.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import { sendEmail } from '../helpers/email'
 import { EmailResponseInterface } from '../interfaces'
+const fs = require('fs')
+const path = require('path')
 const projects = require('../static-responses/portfolio-projects')
 
 export const projectsBuilder = (req: express.Request, res: express.Response, next: express.NextFunction) => res.status(200).send(projects)
@@ -13,4 +15,12 @@ export const contactEmailBuilder = async (req: express.Request, res: express.Res
     } else {
         res.status(400).send({ emailSentSuccess: false })
     }
+}
+
+export const sendPdfWorker = (req: express.Request, res: express.Response, next: express.NextFunction) => res.send(fs.readFileSync(path.join(__dirname, '../helpers/pdf-worker/pdf-worker.min.js')))
+
+export const sendResume = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const pdf = fs.readFileSync(path.join(__dirname, '../helpers/resume.pdf'))
+    res.contentType('application/pdf')
+    res.send(pdf)
 }
--- src/routes/portfolio.ts
+++ src/routes/portfolio.ts
@@ -3,7 +3,8 @@ import cors from 'cors'
 import { corsConfig } from '../configs/cors-config'
 import {
     contactEmailBuilder,
-    projectsBuilder
+    projectsBuilder,
+    sendPdfWorker, sendResume
 } from '../builders/portfolio-builders'
 
 const portfolioRouter = express.Router()
@@ -14,5 +15,7 @@ portfolioRouter.use(express.json())
 
 portfolioRouter.get('/projects', (req: express.Request, res:express.Response, next: express.NextFunction) => projectsBuilder(req, res, next))
 portfolioRouter.post('/contact', async (req: express.Request, res: express.Response, next:express.NextFunction) => await contactEmailBuilder(req, res, next))
+portfolioRouter.get('/pdf-worker', (req: express.Request, res: express.Response, next: express.NextFunction) => sendPdfWorker(req, res, next))
+portfolioRouter.get('/resume', (req: express.Request, res: express.Response, next: express.NextFunction) => sendResume(req, res, next))
 
 export default portfolioRouter
