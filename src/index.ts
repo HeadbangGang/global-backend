605625cfeb5fe0bb04bde009426926d0eefa65e3
Tayden Flitcroft <taydengoodeill@gmail.com>
2022-07-04 23:50:39 -0500

    adding auth0 endpoint security

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import express from 'express'
+import { expressJwtSecret } from 'jwks-rsa'
+const { expressjwt: jwt } = require('express-jwt')
 
 // Routes
 import pokedexRouter from './routes/pokedex'
 import portfolioRouter from './routes/portfolio'
 import { s3 } from './helpers/aws-s3'
-import { AWSError, S3 } from 'aws-sdk'
+import { AWSError } from 'aws-sdk'
 
 const app = express()
 const PORT = process.env.PORT || 3001
-
-app.use('/pokedex', pokedexRouter)
-app.use('/portfolio', portfolioRouter)
 
 app.get('/', async (req:express.Request, res: express.Response) => {
     await s3.getObject({ Bucket: process.env.S3_BUCKET, Key: 'landing-page.html' }, (err: AWSError, data) => {
@@ -21,6 +20,24 @@ app.get('/', async (req:express.Request, res: express.Response) => {
         }
     })
 })
+
+const checkJwt = jwt({
+    secret: expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-fsldf8y6.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'https://api.taydenflitcroft.com',
+    issuer: 'https://dev-fsldf8y6.us.auth0.com/',
+    algorithms: ['RS256']
+})
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(checkJwt)
+}
+app.use('/pokedex', pokedexRouter)
+app.use('/portfolio', portfolioRouter)
 
 app.listen(PORT, () => {
     console.info(`Current Environment: ${process.env.CONFIG_ENV || 'production'}`)
